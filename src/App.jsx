7412925698cb27a6cdd001b9338b92e182e63f4f
Yolosopher <nika.nishnianidze.10@gmail.com>
2024-06-26 22:12:02 +0400

    refactor: optimize website for small screens and add responsive design mixins

--- src/App.jsx
+++ src/App.jsx
@@ -15,41 +15,51 @@ function App() {
   const [swiperInstance, setSwiperInstance] = useState(null);
   return (
     <>
-      <Swiper
-        keyboard={{
-          enabled: true,
-          onlyInViewport: false,
-        }}
-        mousewheel={{
-          enabled: true,
-          releaseOnEdges: true,
-        }}
-        speed={1000}
-        direction="vertical"
-        spaceBetween={0}
-        slidesPerView={1}
-        className="scroll-swiper"
-        modules={[Mousewheel, Keyboard]}
-        onSwiper={(swiper) => {
-          setSwiperInstance(swiper);
-        }}
-      >
-        <SwiperSlide>
-          <Navigation swiperInstance={swiperInstance} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Projects />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Works />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Clients />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Story />
-        </SwiperSlide>
-      </Swiper>
+      <div className="less-than-1650">
+        <h1>
+          This website is not optimized for small screens. Please use a device
+          with a larger screen to view the website.
+          <br />(<span>Minimum width: </span>
+          1650px)
+        </h1>
+      </div>
+      <div className="website">
+        <Swiper
+          keyboard={{
+            enabled: true,
+            onlyInViewport: false,
+          }}
+          mousewheel={{
+            enabled: true,
+            releaseOnEdges: true,
+          }}
+          speed={1000}
+          direction="vertical"
+          spaceBetween={0}
+          slidesPerView={1}
+          className="scroll-swiper"
+          modules={[Mousewheel, Keyboard]}
+          onSwiper={(swiper) => {
+            setSwiperInstance(swiper);
+          }}
+        >
+          <SwiperSlide>
+            <Navigation swiperInstance={swiperInstance} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Projects />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Works />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Clients />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Story />
+          </SwiperSlide>
+        </Swiper>
+      </div>
     </>
   );
 }
